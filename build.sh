af2772194ccbe51507837bacfd87a16ae1680979
Athan Clark <athan.clark@gmail.com>
2023-10-06 07:10:45 -0400

    fixed weird meta description issue and html lang

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ trunk build && \
     sed -i '/<body>/a {{ body }}' docs/index.html && \
     cargo run --features hydration > docs/body.html && \
     ltext 'docs/index.html docs/body.html' --raw docs/body.html > docs/index.html.tmp && \
+    rm docs/body.html && \
     mv docs/index.html.tmp docs/index.html
